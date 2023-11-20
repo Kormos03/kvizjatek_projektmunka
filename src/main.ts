1fed46c46f2a48faa05f14f9b21d63eb4212da45
Petrik Diak <diak@petrik.hu>
2023-11-20 17:45:02 +0100

    question handler skeleton

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import './style.css'
 import questions from './questions.json';
 import scores from './scores.json';
 
-
+const questionArray = [];
+const settings = ["easy", 101, true]; //placeholder settings: difficulty, genre, showsource
 
 function init()
 {
-	//quotes.quotes.forEach(start);
+	questions.forEach(loadQuestions);
 	const question = document.createElement("h1");
 	question.id = "question"
 	question.innerText = "placeholder question"
@@ -14,6 +15,50 @@ function init()
 	
 	for (let i = 1; i < 5; i++) {
 		buttonCreation(i)
+	}
+	
+	questionHandler();
+}
+
+function loadQuestions(item, index)
+{
+	questionArray.push(item);
+}
+
+function questionHandler()
+{
+	let diff;
+	let genre;
+	let source;
+	switch(settings[0]) {
+		case "easy":
+			diff = 1;
+		break;
+		case "normal":
+			diff = 3;
+		break;
+		default:
+			// if something goes wrong, default to hard mode
+			diff = 5;
+	}
+	console.log(genre);
+	switch(settings[1]) {
+		case 101:
+			genre = 1; //genres to be determined: using numbers for now
+		break;
+		case 102:
+			genre = 2;
+		break;
+		default:
+			// if something goes wrong, default to no genre; don't do anything
+	}
+	switch(settings[2]) {
+		case true:
+			source = true;
+		break;
+		default:
+			// if something goes wrong, default to no source
+			source = false;
 	}
 }
 
