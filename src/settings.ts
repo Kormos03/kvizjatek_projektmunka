69132449d029b5496511024a121c5a1a80293373
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-11-27 17:18:17 +0100

    settings sliders default true

---
+++ src/settings.ts
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toriSlider = document.getElementById('tori_slider') as HTMLInputElement;
+    toriSlider.checked = true;
+
+    const matekSlider = document.getElementById('matek_slider') as HTMLInputElement;
+    matekSlider.checked = true;
+
+    const infoSlider = document.getElementById('info_slider') as HTMLInputElement;
+    infoSlider.checked = true;
+
+    const bioszSlider = document.getElementById('biosz_slider') as HTMLInputElement;
+    bioszSlider.checked = true;
+
+});
