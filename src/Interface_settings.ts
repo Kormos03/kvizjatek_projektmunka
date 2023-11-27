eb9fd194430228ce2e0453fc4d7555fdfc54e0e5
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-11-27 18:02:47 +0100

    settings class default values created

---
+++ src/Interface_settings.ts
@@ -0,0 +1,8 @@
+export interface Isettings {
+    tori: boolean,
+    matek: boolean,
+    info: boolean,
+    biosz: boolean,
+    fizika: boolean,
+    kemia: boolean
+}
--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,30 @@
+import { Isettings } from './Interface_settings'
+class Settings implements Isettings {
+    tori: boolean = (document.getElementById('tori_slider')! as HTMLInputElement).checked;
+    matek: boolean = (document.getElementById('matek_slider')! as HTMLInputElement).checked;
+    info: boolean = (document.getElementById('info_slider')! as HTMLInputElement).checked;
+    biosz: boolean = (document.getElementById('biosz_slider')! as HTMLInputElement).checked;
+    fizika: boolean = (document.getElementById('fizika_slider')! as HTMLInputElement).checked;
+    kemia: boolean = (document.getElementById('kemia_slider')! as HTMLInputElement).checked;
+
+    constructor() {
+        this.tori = (document.getElementById('tori_slider')! as HTMLInputElement).checked = true;
+        this.matek = (document.getElementById('matek_slider')! as HTMLInputElement).checked = true;
+        this.info = (document.getElementById('info_slider')! as HTMLInputElement).checked = true;
+        this.biosz = (document.getElementById('biosz_slider')! as HTMLInputElement).checked = true;
+        this.fizika = (document.getElementById('fizika_slider')! as HTMLInputElement).checked = true;
+        this.kemia = (document.getElementById('kemia_slider')! as HTMLInputElement).checked = true;
+
+    }
+}
+
+const settings = new Settings();
+/**
+ * Default settings
+ */
 document.addEventListener('DOMContentLoaded', () => {
-    const toriSlider = document.getElementById('tori_slider') as HTMLInputElement;
-    toriSlider.checked = true;
 
-    const matekSlider = document.getElementById('matek_slider') as HTMLInputElement;
-    matekSlider.checked = true;
+    console.log(settings);
 
-    const infoSlider = document.getElementById('info_slider') as HTMLInputElement;
-    infoSlider.checked = true;
-
-    const bioszSlider = document.getElementById('biosz_slider') as HTMLInputElement;
-    bioszSlider.checked = true;
 
 });
