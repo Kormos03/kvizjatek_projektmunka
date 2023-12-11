e873b6f1e38f24e8f53908829cb3d35849f5eca7
Kormos Áron <104300544+Kormos03@users.noreply.github.com>
2023-12-11 17:45:28 +0100

    index.html fixed

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,3 @@ document.getElementById("maingame").addEventListener("click", () => { localStora
 document.getElementById("random").addEventListener("click", () => { localStorage.setItem('mode', 'random') })
 document.getElementById("timed").addEventListener("click", () => { localStorage.setItem('mode', 'timed') })
 // sends gamemode information cross-page
-
-console.log(genreSlider)
